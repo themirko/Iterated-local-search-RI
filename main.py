b05a4456839b0dcb7f8e220386c358d520f2b727
Dusan Mirkovic <mirkovicdusan2001@gmail.com>
2025-10-01 14:34:54 +0200

    added couple of functions

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import tsplib95 as tsp
 import random
 
+def fitnessFunction(tour, problem):
+  return sum(problem.get_weight(tour[i], 
+                                tour[(i + 1) % len(tour)])
+                                for i in range(len(tour)))
+
 def GenerateInitialSolution(problem):
   nodes = list(problem.get_nodes())
   random.shuffle(nodes)
@@ -8,27 +13,37 @@ def GenerateInitialSolution(problem):
   return nodes
 
 def doubleBridgePerturbation(tour):
-    n = len(tour)
+  n = len(tour)
+  if n <= 8:
+    return tour
 
-    if n <= 8:
-       return tour
-    
-    idx = sorted(random.sample(range(n), 4))
-    i, j, k, l = idx
+  i, j, k, l = sorted(random.sample(range(n), 4))
+  return tour[:i] + tour[k:l] + tour[j:k] + tour[i:j] + tour[l:]
 
-    new_tour = []
-    new_tour.extend(tour[:i])
-    new_tour.extend(tour[k:l])
-    new_tour.extend(tour[j:k])
-    new_tour.extend(tour[i:j])
-    new_tour.extend(tour[l:])
 
-    return new_tour
+def twoOptLocalSearch(tour):
+  n = len(tour)
+  
+  best_tour = tour
+  best_fitness = fitnessFunction(best_tour)
 
+  for i in range(1, n-1):
+    for j in range(i+2, n):
+
+      new_tour = tour[:i] + tour[i:j][::-1] + tour[j:]
+      new_fitness = fitnessFunction(new_tour)
+
+      if new_fitness < best_fitness:
+        return new_tour
+
+  return best_tour
 
 if __name__ == "__main__":
-  problem = tsp.load('ALL_tsp/pcb442.tsp/pcb442.tsp')  
+  problem = tsp.load('ALL_tsp/pcb442.tsp/pcb442.tsp')
   tour = GenerateInitialSolution(problem)
+
   print("Generisana tura (prvih 20 čvorova):", tour[:20])
+  print("Dužina inicijalne ture:", fitnessFunction(tour, problem))
 
   tour = doubleBridgePerturbation(tour)
+  print("Dužina posle perturbacije:", fitnessFunction(tour, problem))
