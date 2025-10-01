68b59d87c7f31402503b451d7588d633d0baa13d
lenafod <lena.fodulovic01@gmail.com>
2025-10-01 15:30:20 +0200

    deleted files

--- utils.py
+++
@@ -1,6 +0,0 @@
-import math
-
-def fitnessFunction(tour, problem):
-  return sum(problem.get_weight(tour[i], 
-                                tour[(i + 1) % len(tour)])
-                                for i in range(len(tour)))
